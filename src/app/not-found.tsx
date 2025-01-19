4407b10fb29ad8c37c001e37d05815b670b3c4ca
__filename <filename@inft.kr>
2025-01-19 22:29:32 +0800

    feat: added 404 500 easter egg

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@/components/navbar'
 import { buttonVariants } from '@/components/ui/button'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function NotFound() {
@@ -7,9 +8,14 @@ export default function NotFound() {
     <>
       <Navbar />
       <main className="flex h-full min-h-[90vh] w-full flex-col items-center justify-center gap-8 p-4">
-        <h1 className="scroll-m-20 font-extrabold text-4xl tracking-tight lg:text-5xl">
-          404
-        </h1>
+        <Image
+          src="/NotFound.png"
+          alt="Not found"
+          width={512}
+          height={512}
+          sizes="100vw"
+          className="lg:w-2/6"
+        />
         <p>Уучлаарай, хуудас олдсонгүй</p>
 
         <Link href="/dash" className={buttonVariants({ variant: 'default' })}>
