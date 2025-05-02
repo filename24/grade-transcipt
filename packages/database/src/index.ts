89be5816bdb12e8d7b4a436d21e22d92289e6b73
__filename <filename@inft.kr>
2025-05-02 23:24:54 +0800

    refactor: comment out unused PrismaClient import and output path

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,7 @@
-import { PrismaClient } from '../generated/prisma'
+// import { PrismaClient } from '../generated/prisma'
+
+import { PrismaClient } from '@prisma/client'
+
 // import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
@@ -9,4 +12,5 @@ export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export * from '../generated/prisma'
+// export * from '../generated/prisma'
+export * from '@prisma/client'
