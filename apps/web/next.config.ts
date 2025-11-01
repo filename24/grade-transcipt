84b5cc8cb240644cf513cf41baa72f1d37b10cbd
__filename <filename@inft.kr>
2025-11-02 01:16:08 +0800

    chore(deps): updated nextjs 16

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,9 +4,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true
-  },
   logging: {
     fetches: {
       fullUrl: true
