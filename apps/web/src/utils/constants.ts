485919b9693ff3f5d41633019789e480714a338f
Ankhgerel Amgalantamir <filename@inft.kr>
2026-09-12 20:48:22 +0900

    Update constants.ts

--- apps/web/src/utils/constants.ts
+++ apps/web/src/utils/constants.ts
@@ -86,4 +86,4 @@ export type EducationLevel = keyof typeof SEMESTER_DATE
 export const RedisKeys = {
   esisToken: 'gt.knea.esis.token'
 } as const
-export const CDN_ENDPOINT = 'https://cdn.flnm.dev/grade-transcript'
+export const CDN_ENDPOINT = 'https://cdn.flnm.me/grade-transcript'
